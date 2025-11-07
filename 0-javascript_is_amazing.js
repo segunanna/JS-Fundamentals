8c66b4f585d75c4e1dd78593efded159c87469bd
segunanna <annasegun8@gmail.com>
2025-11-07 09:52:03 +0100

    initial commit

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,5 +1,15 @@
-// 0-javascript_is_amazing.js
-console.log ("JavaScript is amazing")
+/**
+ * Script that defines a constant variable and prints its value to the console.
+ *
+ * Constraints:
+ * 1. Must use a constant variable called 'myVar'.
+ * 2. Value must be "JavaScript is amazing".
+ * 3. Must use console.log().
+ * 4. Cannot use 'var'.
+ */
 
-// variables
-Myvar = "JavaScript is amazing"
+// Create the constant variable 'myVar' with the required string value.
+const myVar = "JavaScript is amazing";
+
+// Print the value of the constant variable to the console.
+console.log(myVar);
---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+/**
+ * Script that prints three distinct lines of text using console.log()
+ * and without using the 'var' keyword.
+ */
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
