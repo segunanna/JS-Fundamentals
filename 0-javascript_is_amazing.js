51b755e5c4c8ca8fd5f4b4bd27b67a93687af56d
segunanna <annasegun8@gmail.com>
2025-11-08 06:57:58 +0100

    initial commit

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,15 +1,5 @@
-/**
- * Script that defines a constant variable and prints its value to the console.
- *
- * Constraints:
- * 1. Must use a constant variable called 'myVar'.
- * 2. Value must be "JavaScript is amazing".
- * 3. Must use console.log().
- * 4. Cannot use 'var'.
- */
+#!/usr/bin/node
 
-// Create the constant variable 'myVar' with the required string value.
-const myVar = "JavaScript is amazing";
-
-// Print the value of the constant variable to the console.
+const myVar = 'JavaScript is amazing';
 console.log(myVar);
+
