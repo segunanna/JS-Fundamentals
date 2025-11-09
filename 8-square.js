883c56f54d5329c31e9bc658f9b52568033b8691
segunanna <annasegun8@gmail.com>
2025-11-09 19:05:07 +0100

    Initial Commit

--- 8-square.js
+++ 8-square.js
@@ -1,12 +1,9 @@
-#!/usr/bin/node
 const size = parseInt(process.argv[2]);
 
-console.log('X'.repeat(size));
-
 if (isNaN(size)) {
-  console.log('Missing size');
-} else if (size > 0) {
+  console.log("Missing size");
+} else {
   for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+    console.log("X".repeat(size));
   }
 }
